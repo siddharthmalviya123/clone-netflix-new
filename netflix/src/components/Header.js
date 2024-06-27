f260b52bf22c4e4da85f291456a7cf579b693acc
siddharthmalviya123 <siddharth.malviya@practo.com>
2024-06-27 05:59:45 +0530

    login page added

--- netflix/src/components/Header.js
+++ netflix/src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react'
-
+import { IoIosArrowDropdown } from "react-icons/io";
 const Header = () => {
   return (
     <div className='absolute z-10 flex w-full items-center justify-between px-6 bg-gradient-to-b from-black'>
             <img className='w-56' src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1198px-Netflix_2015_logo.svg.png" alt="netflix-logo" />
                 <div className='flex items-center'>
+                <IoIosArrowDropdown size="24px" color='white' />
                     <h1 className='text-lg font-medium text-white'>psiddafasdf</h1>
                     <div className='ml-4'>
                     <button className='bg-red-800 text-white px-4 py-2'>logout</button>
--- netflix/src/components/Login.js
+++ netflix/src/components/Login.js
@@ -3,11 +3,12 @@ import Header from './Header'
 const Login = () => {
   return (
     <div>
-      <Header/>
-      <div>
-        login
-      </div>
-    </div>
+            <Header />
+            <div className='absolute'>
+                <img className='w-[100vw] h-[100vh] bg-cover' src="https://assets.nflxext.com/ffe/siteui/vlv3/dc1cf82d-97c9-409f-b7c8-6ac1718946d6/14a8fe85-b6f4-4c06-8eaf-eccf3276d557/IN-en-20230911-popsignuptwoweeks-perspective_alpha_website_medium.jpg" alt="banner" />
+            </div>
+            
+        </div>
   )
 }
 
