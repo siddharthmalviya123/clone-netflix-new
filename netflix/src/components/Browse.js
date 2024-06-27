ec1f7978f1fd2105169500b2581ba4f8ec4f43b4
siddharthmalviya123 <siddharth.malviya@practo.com>
2024-06-28 01:27:02 +0530

    tmbd and movie fetching setup comp

--- netflix/src/components/Browse.js
+++ netflix/src/components/Browse.js
@@ -1,21 +1,35 @@
 import React, { useEffect } from 'react'
 import Header from './Header'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom';
 import MainContainer from './MainContainer';
 import MovieContainer from './MovieContainer';
-
+import { now_playing_movies ,options} from '../utils/constant';
+import axios from 'axios';
+import { getNowPlayingMovies } from '../redux/movieSlice';
 
 const Browse = () => {
 const user = useSelector(store=>store.app.user);
 const navigate = useNavigate();
+const dispatch= useDispatch();
 
+const nowplayingmovies = async()=>{
+  try{
+    const res = await axios.get(now_playing_movies , options)
+    console.log(res.data.results);
+    dispatch(getNowPlayingMovies(res.data.results))
+  }
+  catch(error)
+  {
+    console.log(error)
+  }
+}
 useEffect(()=>{
   if(!user)
     {
       navigate("/");
     }
-  
+  nowplayingmovies();
 },[])
 
   return (
--- netflix/src/utils/constant.js
+++ netflix/src/utils/constant.js
@@ -1,1 +1,11 @@
 export const API_END_POINT = "http://localhost:8000/api/v1/user";
+
+export const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3OTI3NzRmNDhmZTkzOTcwMGQ4NGQ1ZmFiYTU3MmU1YiIsIm5iZiI6MTcxOTQ3MTY2OC4xMDIzODUsInN1YiI6IjY2N2QwZDFiN2JjNTJjMjQyZDE5NGEwMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.e25FdBjPllLXXsY1P2xRKaA9l2gzJhabdd1GbkAuU1E'
+    }
+  };
+
+  export const now_playing_movies="https://api.themoviedb.org/3/movie/now_playing"
