ac09baa1a507d5d87a35fca13b64617582669a43
siddharthmalviya123 <siddharth.malviya@practo.com>
2024-06-27 05:51:30 +0530

    new features added

---
+++ netflix/src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='absolute z-10 flex w-full items-center justify-between px-6 bg-gradient-to-b from-black'>
+            <img className='w-56' src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1198px-Netflix_2015_logo.svg.png" alt="netflix-logo" />
+                <div className='flex items-center'>
+                    <h1 className='text-lg font-medium text-white'>psiddafasdf</h1>
+                    <div className='ml-4'>
+                    <button className='bg-red-800 text-white px-4 py-2'>logout</button>
+                    <button className='bg-red-800 text-white px-4 py-2 ml-2'> search movie</button>
+                    </div>
+                  
+                </div>
+        </div>
+  )
+}
+
+export default Header
---
+++ netflix/src/components/Login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Header'
+const Login = () => {
+  return (
+    <div>
+      <Header/>
+      <div>
+        login
+      </div>
+    </div>
+  )
+}
+
+export default Login
